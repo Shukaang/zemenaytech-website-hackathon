914473c443394027df716bd6c1eecf5152128daf
Shukaang <shuebahmed32@gmail.com>
2025-08-15 16:04:19 +0300

    Fix

--- src/components/about/meet-our-team.tsx
+++ src/components/about/meet-our-team.tsx
@@ -61,7 +61,7 @@ export function MeetOurTeam() {
         </div>
 
         {/* Team Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 max-w-6xl mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto">
           {teamMembers.map((member, index) => (
             <Card
               key={index}
